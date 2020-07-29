3de784b637943204eeae6eaa4d08df84b3c865f1
toddkarin <toddkarin@lbl.gov>
2020-07-29 15:41:44 -0700

    Move header code

--- pvterms/main.py
+++ pvterms/main.py
@@ -1,22 +1,8 @@
 import pandas as pd
 import os
+from pvterms.constants import replacement as _replacement
 
-# Import pv terms from definitions.csv
-_dir_path = os.path.dirname(os.path.realpath(__file__))
-_filename_defs = os.path.join(_dir_path, 'definitions.csv')
-
-terms = pd.read_csv(_filename_defs,skiprows=[0,1])
-terms.fillna('',inplace=True)
-
-# Make a replacement dictionary
-_replacement = {}
-for k in terms.index:
-    if terms.loc[k,'Deprecated']:
-        deprecated_terms = terms.loc[k, 'Deprecated'].split(',')
-        standard_term = terms.loc[k, 'Parameter']
-        for p in deprecated_terms:
-            _replacement[p] = standard_term
-
+# TODO: rename source as docs_source
 def rename(x):
     """
     Rename a variable into the pv-terms scheme. Auto-detect which type of
